69ff980ca23bbbdc105c987e947aeea9a9502edf
Farhan Asif <f.asif2010@gmail.com>
2019-12-10 18:18:39 +0600

    order routes update

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,24 +2,48 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const checkAuth = require('../middleware/check-auth');
+var randomstring = require("randomstring");
 
 const Order = require('../models/order');
 const Product = require('../models/product');
+const OrderDetail = require('../models/orderDetail');
 
 router.get('/', checkAuth, (req, res, next) => {
-    Order.find()
-        .select('product quantity _id')
-        .populate('product', 'name price _id')
+
+    Order.aggregate([{
+            $lookup: {
+                from: "orderdetails", // collection name in db
+                localField: "_id",
+                foreignField: "order",
+                as: "orderdetails"
+            }
+        }, {
+            $lookup: {
+                from: "users", // collection name in db
+                localField: "user",
+                foreignField: "_id",
+                as: "user"
+            }
+        }])
         .exec()
         .then(docs => {
-            //console.log(docs);
             const response = {
                 count: docs.length,
                 orders: docs.map(doc => {
                     return {
-                        product: doc.product,
-                        quantity: doc.quantity,
+                        amount: doc.amount,
+                        shippingAddress: doc.shippingAddress,
+                        city: doc.city,
+                        state: doc.state,
+                        zip: doc.zip,
+                        country: doc.country,
+                        payment: doc.payment,
+                        tracking: doc.tracking,
+                        status: doc.status,
                         _id: doc._id,
+                        user: doc.user,
+                        items: doc.orderdetails.length,
+                        details: doc.orderdetails,
                         request: {
                             type: 'GET',
                             url: 'http://localhost:4000/orders/' + doc._id
@@ -28,8 +52,10 @@ router.get('/', checkAuth, (req, res, next) => {
                 })
             }
             res.status(200).json(response);
+
         })
         .catch(err => {
+            console.log(err);
             res.status(500).json({
                 error: err
             });
@@ -37,53 +63,115 @@ router.get('/', checkAuth, (req, res, next) => {
 });
 
 router.post('/', checkAuth, (req, res, next) => {
-    Product.findById(req.body.productid)
-        .then(product => {
-            if (!product) {
-                res.status(500).json({
-                    message: 'Product not found'
-                })
-            }
-            const order = new Order({
-                _id: mongoose.Types.ObjectId(),
-                quantity: req.body.quantity,
-                product: req.body.productid
-            });
 
-            return order.save()
+    if (req.body.products.length > 1) {
+        const tracking = randomstring.generate(18);
 
-        })
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'It works on POST',
-                createdP: result
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'Product not found',
-                error: err
-            })
+        const order = new Order({
+            _id: mongoose.Types.ObjectId(),
+            user: req.body.user,
+            amount: req.body.amount,
+            shippingAddress: req.body.shippingAddress,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip,
+            country: req.body.country,
+            payment: 1,
+            tracking: tracking,
+            status: 'SUBMITTED'
         });
 
+        order.save()
+            .then(result => {
+                console.log(result._id);
+
+                var orders = [];
+
+                for (var i = 0; i < req.body.products.length; i++) {
+                    //console.log(req.body.products[i]._id + ' - ' + req.body.products[i].quantity + ' - ' + result._id);
+                    orders.push({
+                        order: result._id,
+                        product: req.body.products[i]._id,
+                        quantity: req.body.products[i].quantity
+                    })
+                }
+                console.log(orders);
+
+                OrderDetail.insertMany(orders, (err, odetails) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        res.status(200).json({
+                            message: 'request received',
+                            result: result,
+                            odetails: odetails
+                        });
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            });
+    } else {
+        res.status(200).json({
+            message: 'No Product submitted'
+        });
+    }
 });
 
 router.get('/:orderId', checkAuth, (req, res, next) => {
     const id = req.params.orderId;
-    Order.findById(id)
-        .select('product quantity _id')
-        .populate('product', 'name price _id')
-        .exec()
-        .then(doc => {
-            console.log(doc);
-            if (doc) {
-                res.status(200).json(doc);
-            } else {
-                res.status(404).json({
-                    message: 'No entry found!'
-                });
+    Order.aggregate([
+            { $match: { _id: mongoose.Types.ObjectId(id) } },
+            {
+                $lookup: {
+                    from: "orderdetails", // collection name in db
+                    localField: "_id",
+                    foreignField: "order",
+                    as: "orderdetails"
+                }
+            },
+            {
+                $lookup: {
+                    from: "users", // collection name in db
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user"
+                }
             }
+        ])
+        .exec()
+        .then(docs => {
+            const response = {
+                orderdetails: docs.map(doc => {
+                    return {
+                        amount: doc.amount,
+                        shippingAddress: doc.shippingAddress,
+                        city: doc.city,
+                        state: doc.state,
+                        zip: doc.zip,
+                        country: doc.country,
+                        payment: doc.payment,
+                        tracking: doc.tracking,
+                        status: doc.status,
+                        _id: doc._id,
+                        user: doc.user,
+                        items: doc.orderdetails.length,
+                        details: doc.orderdetails,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:4000/orders/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
 
         })
         .catch(err => {
